88ae04540227b419dd637c66b8a0cd36f26bf848
Leo Ng <38610197+LilLeoNeko@users.noreply.github.com>
2019-10-23 15:02:39 +1100

    Add project files.

---
+++ DoGrooming/Controllers/ProviderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoGrooming.Models;
+using DoGrooming.Views.ViewModel;
+
+
+namespace DoGrooming.Controllers
+{
+    public class ProviderController : Controller
+    {
+        // GET: Provider
+        public ActionResult Display()
+        {
+            var provider = new Provider() { Name = "hello" };
+            var services = new List<Service>
+            {
+                new Service {Name = "Clean"},
+                new Service {Name = "haircut"},
+                new Service {Name = "nail clipping"}
+            };
+            var viewModel = new ProviderServiceViewModel
+            {
+                Provider = provider,
+                Services = services
+            };
+            return View(viewModel);
+        }
+        public ActionResult About(int id)
+        {   
+            return Content(""+id);
+        }
+    }
+}
---
+++ DoGrooming/Views/ViewModel/ProviderServiceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DoGrooming.Models;
+
+namespace DoGrooming.Views.ViewModel
+{
+    public class ProviderServiceViewModel
+    {
+        public Provider Provider { get; set; }
+        public List<Service> Services { get; set; }
+
+    }
+}
