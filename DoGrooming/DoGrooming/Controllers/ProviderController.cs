ab982045ad7bfccdae967cbe81a005cf3b4f66c6
Leo Ng <38610197+LilLeoNeko@users.noreply.github.com>
2019-10-23 17:55:30 +1100

    Upload

---
+++ DoGrooming/DoGrooming/Controllers/ProviderController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoGrooming.Models;
+using DoGrooming.ViewModels;
+
+namespace DoGrooming.Controllers
+{
+    public class ProviderController : Controller
+    {
+        // GET: Provider
+        public ActionResult Display()
+        {
+            var provider = new Provider {Name = "Number1"};
+            var services = new List<Service>
+            {
+                new Service {Name= "Hair Cut"},
+                new Service {Name = "Body Clean"}
+            };
+            var content = new ProviderServiceViewModel
+            {
+                Provider = provider,
+                Services = services
+            };
+            return View(content);
+        }
+        [Route("provider/{id}/{name}")]
+        public ActionResult Test(int id, string name)
+        {
+            return Content(id + "/" + name);
+        }
+    }
+}
---
+++ DoGrooming/DoGrooming/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoGrooming.Startup))]
+namespace DoGrooming
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
